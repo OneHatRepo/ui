8b322f09e53f2dbef34433e961f63741e06ac0be
Scott Spuler <scott@onehat.com>
2023-02-23 08:49:46 -0800

    fix registerWebComponents, bump revision

--- src/functions/registerWebComponents.js
+++ src/functions/registerWebComponents.js
@@ -1,5 +1,5 @@
 import UiGlobals from '../UiGlobals.js';
-import CKEditor from '../PlatformImports/Web/CKEditor';
+import CKEditor from '../PlatformImports/Web/CKEditor.js';
 import Datetime from '../PlatformImports/Web/Datetime.js';
 import Draggable from '../PlatformImports/Web/Draggable.js';
 import File from '../Components/Form/Field/File.js';
