cdf0282556cf02f16de32cfcb5523038f52dccd8
Scott Spuler <scott@onehat.com>
2023-03-25 07:50:32 -0700

    Toggle shows text readout

--- src/components/Form/Field/Toggle.js
+++ src/components/Form/Field/Toggle.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
+	Row,
 	Switch,
+	Text,
 } from 'native-base';
 import UiGlobals from '../../../UiGlobals.js';
 import withTooltip from '../../Hoc/withTooltip.js';
@@ -19,21 +21,24 @@ const
 				setValue(!value);
 			};
 
-		return <Switch
-					ref={props.outerRef}
-					onToggle={onToggle}
-					isChecked={!!value}
-					// flex={1}
-					bg={styles.FORM_TOGGLE_BG}
-					size={styles.FORM_TOGGLE_SIZE}
-					onTrackColor={styles.FORM_TOGGLE_ON_COLOR}
-					offTrackColor={styles.FORM_TOGGLE_OFF_COLOR}
-					_hover={{
-						onTrackColor: styles.FORM_TOGGLE_ON_HOVER_COLOR,
-						offTrackColor: styles.FORM_TOGGLE_OFF_HOVER_COLOR,
-					}}
-					{...propsToPass}
-				/>;
+		return <Row alignItems="center">
+					<Switch
+						ref={props.outerRef}
+						onToggle={onToggle}
+						isChecked={!!value}
+						// flex={1}
+						bg={styles.FORM_TOGGLE_BG}
+						size={styles.FORM_TOGGLE_SIZE}
+						onTrackColor={styles.FORM_TOGGLE_ON_COLOR}
+						offTrackColor={styles.FORM_TOGGLE_OFF_COLOR}
+						_hover={{
+							onTrackColor: styles.FORM_TOGGLE_ON_HOVER_COLOR,
+							offTrackColor: styles.FORM_TOGGLE_OFF_HOVER_COLOR,
+						}}
+						{...propsToPass}
+					/>
+					<Text fontSize={styles.FORM_TOGGLE_FONTSIZE}>{!!value ? 'Yes' : 'No'}</Text>
+				</Row>;
 	},
 	ToggleField = withValue(ToggleElement);
 
