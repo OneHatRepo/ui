76149de1048e3dc43a97accc720f7fe6d90b88fb
Scott Spuler <scott@onehat.com>
2023-05-23 11:42:19 -0700

    fix useWindowSize

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,16 +1,25 @@
 // from https://designcode.io/react-hooks-usewindowsize-hook
 
 import { useLayoutEffect, useState } from 'react';
+import _ from 'lodash';
 
 // For web only!
 export default function useWindowSize() {
-	const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight
+	});
 
 	const handleSize = () => {
-		setWindowSize({
-			width: window.innerWidth,
-			height: window.innerHeight
-		});
+		const
+			existingSize = windowSize,
+			newSize = {
+				width: window.innerWidth,
+				height: window.innerHeight
+			};
+		if (!_.isEqual(existingSize, newSize)) {
+			setWindowSize(newSize);
+		}
 	};
 
 	useLayoutEffect(() => {
