9c973e5c0035669ba35bd87a75cc42678bfeacc8
Scott Spuler <scott@onehat.com>
2023-05-24 11:17:13 -0700

    WaitMessage, bump revision

--- src/components/Messages/WaitMessage.js
+++ src/components/Messages/WaitMessage.js
@@ -1,4 +1,6 @@
- import {
+import {
+	Button,
+	Modal,
 	Row,
 	Text,
 } from 'native-base';
@@ -9,10 +11,20 @@ export default function WaitMessage(props) {
 			textMessage = 'Please wait...',
 		} = props;
 
-	return <Row position="absolute" width="100%" top="40" left="0" _backdrop={{ bg: "#000" }} justifyContent="center" alignItems="center" pb={20} {...props}>
-				<Row bg="#fff" p={3} width="50%" justifyContent="center" alignItems="center" borderColor="#000" borderRadius={5}>
-					<Loading minHeight="auto" h={5} w={5} mr={2} />
-					<Text color="#000">{textMessage}</Text>
-				</Row>
-			</Row>;
+	return <Modal {...props} _backdrop={{ bg: "#000" }}>
+				<Modal.Content>
+					<Modal.Body
+						borderTopWidth={0}
+						bg="#fff"
+						p={3}
+						justifyContent="center"
+						alignItems="center"
+						borderRadius={5}
+						flexDirection="row"
+					>
+						<Loading minHeight="auto" h={5} w={5} mr={2} />
+						<Text color="#000">{textMessage}</Text>
+					</Modal.Body>
+				</Modal.Content>
+			</Modal>;
 }
