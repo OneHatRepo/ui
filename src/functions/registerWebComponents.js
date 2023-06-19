7ca816c47f2558d440d19871b57cd252c982cb2a
Scott Spuler <scott@onehat.com>
2023-06-19 08:48:36 -0700

    Disable CKEditor.js for now, bump revision

--- src/functions/registerWebComponents.js
+++ src/functions/registerWebComponents.js
@@ -1,5 +1,5 @@
 import UiGlobals from '../UiGlobals.js';
-import CKEditor from '../Components/Form/Field/CKEditor/CKEditor.js';
+// import CKEditor from '../Components/Form/Field/CKEditor/CKEditor.js';
 import Datetime from '../PlatformImports/Web/Datetime.js';
 import Draggable from '../PlatformImports/Web/Draggable.js';
 import File from '../Components/Form/Field/File.js';
@@ -7,7 +7,7 @@ import _ from 'lodash';
 
 export default function registerWebComponents() {
 	_.merge(UiGlobals.components, {
-		CKEditor,
+		// CKEditor, // The CKEditor was giving me CSS import errors, so had to disable it until I can fix those
 		Datetime,
 		Draggable,
 		File,
