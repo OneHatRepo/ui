493a646daabc363b5bacd1b50ac7a3c89ad30492
Scott Spuler <scott@onehat.com>
2023-06-20 12:20:22 -0700

    bump dependencies, revision

--- src/components/Hoc/withEditor.js
+++ src/components/Hoc/withEditor.js
@@ -58,14 +58,17 @@ export default function withEditor(WrappedComponent) {
 				}
 
 				// Set repository to sort by id DESC and switch to page 1, so this new entity is guaranteed to show up on the current page, even after saving
-				Repository.pauseEvents();
-				Repository.sort(Repository.schema.model.idProperty, 'DESC');
-				Repository.setPage(1);
-				Repository.resumeEvents();
-				await Repository.reload();
+				const currentSorter = Repository.sorters[0];
+				if (currentSorter.name !== Repository.schema.model.idProperty || currentSorter.direction !== 'DESC') {
+					Repository.pauseEvents();
+					Repository.sort(Repository.schema.model.idProperty, 'DESC');
+					Repository.setPage(1);
+					Repository.resumeEvents();
+					await Repository.reload();
+				}
 
 				const entity = await Repository.add(addValues, false, true, true);
-				setSelection([entity]);
+				setSelection([entity]); 
 				setIsEditorViewOnly(false);
 				setEditorMode(EDITOR_MODE__ADD);
 				setIsEditorShown(true);
