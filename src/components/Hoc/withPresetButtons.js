59fe87b354adebf87ef4763cc1707305ebd8c641
Scott Spuler <scott@onehat.com>
2023-09-11 08:23:20 -0700

    withPresetButtons - disable Edit button if side editor

--- src/components/Hoc/withPresetButtons.js
+++ src/components/Hoc/withPresetButtons.js
@@ -87,7 +87,7 @@ export default function withPresetButtons(WrappedComponent, isGrid = false) {
 						}
 						break;
 					case 'edit':
-						if (disableEdit || canEditorViewOnly) {
+						if (disableEdit || canEditorViewOnly || isSideEditor) {
 							isDisabled = true;
 						}
 						break;
