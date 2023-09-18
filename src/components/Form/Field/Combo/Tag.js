d6772221bf339ddcaf98b5886e511f6b0125e2cf
Scott Spuler <scott@onehat.com>
2023-09-18 08:04:25 -0700

    disable pagination on Tag

--- src/components/Form/Field/Combo/Tag.js
+++ src/components/Form/Field/Combo/Tag.js
@@ -7,6 +7,10 @@ export default function Tag(props) {
 	return <Combo
 				selectionMode={SELECTION_MODE_MULTI}
 				disableDirectEntry={true}
+				allowToggleSelection={true}
+				disablePagination={true}
+				disableAdjustingPageSizeToHeight={true}
+				pageSize={1000}
 				{...props}
 			/>;
 }
