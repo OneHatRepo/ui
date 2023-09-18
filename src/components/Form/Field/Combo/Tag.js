142022248e4d384da1dec43394eef1c357f34d7e
Scott Spuler <scott@onehat.com>
2023-09-18 15:33:07 -0700

    TagEditor

--- src/components/Form/Field/Combo/Tag.js
+++ src/components/Form/Field/Combo/Tag.js
@@ -1,19 +1,26 @@
 import {
 	SELECTION_MODE_MULTI,
 } from '../../../../Constants/Selection.js';
-import Combo from './Combo.js';
+import Combo, { ComboEditor } from './Combo.js';
 
-export default function Tag(props) {
-	return <Combo
-				selectionMode={SELECTION_MODE_MULTI}
-				valueIsAlwaysArray={true}
-				valueAsIdAndText={true}
-				valueAsStringifiedJson={true}
-				disableDirectEntry={true}
-				allowToggleSelection={true}
-				disablePagination={true}
-				disableAdjustingPageSizeToHeight={true}
-				pageSize={500}
-				{...props}
-			/>;
+function withAdditionalProps(WrappedComponent) {
+	return (props) => {
+		return <WrappedComponent
+					selectionMode={SELECTION_MODE_MULTI}
+					valueIsAlwaysArray={true}
+					valueAsIdAndText={true}
+					valueAsStringifiedJson={true}
+					disableDirectEntry={true}
+					allowToggleSelection={true}
+					disablePagination={true}
+					disableAdjustingPageSizeToHeight={true}
+					pageSize={500}
+					{...props}
+				/>;
+	};
 }
+
+const Tag = withAdditionalProps(Combo);
+export const TagEditor = withAdditionalProps(ComboEditor);
+
+export default Tag;
