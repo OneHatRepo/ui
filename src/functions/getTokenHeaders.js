7f7867d069750f9c4749807eba070e8152dcf7db
Scott Spuler <scott@onehat.com>
2023-09-27 14:04:13 -0700

    fix getTokenHeaders, bump revision

--- src/functions/getTokenHeaders.js
+++ src/functions/getTokenHeaders.js
@@ -2,10 +2,10 @@ import UiGlobals from '../UiGlobals.js';
 
 export default function(clearAll = false) {
 	const reduxState = UiGlobals.redux.getState();
-	if (!reduxState.App.user || (!reduxState.App.user.token && !reduxState.App.user.users__token)) {
+	if (!reduxState.app.user || (!reduxState.app.user.token && !reduxState.app.user.users__token)) {
 		return {};
 	}
-	const token = reduxState.App.user.token || reduxState.App.user.users__token;
+	const token = reduxState.app.user.token || reduxState.app.user.users__token;
 	return {
 		Authentication: clearAll ? null : `Token ${token}`,
 		// Cookie: null,
