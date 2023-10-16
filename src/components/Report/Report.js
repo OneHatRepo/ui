b6c318243e19a081aea15b3257b693803cf56246
Scott Spuler <scott@onehat.com>
2023-10-16 06:51:39 -0700

    Report WIP

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import {
 	Column,
 	Icon,
 	Row,
 	Text,
 } from 'native-base';
-import Button from '../Buttons/Button';
-import Footer from '../Layout/Footer.js';
-import Pdf from '../Icons/Pdf.js';
 import { EDITOR_TYPE__PLAIN } from '../../Constants/Editor';
 import {
 	UI_MODE_WEB,
 	CURRENT_MODE,
 } from '../../Constants/UiModes.js';
+import Form from '../Form/Form.js';
+import withComponent from '../Hoc/withComponent.js';
+import ChartLine from '../Icons/ChartLine.js';
+import Pdf from '../Icons/Pdf.js';
 // import Excel from '../Icons/Excel.js'; // TODO: make this icon
 import UiGlobals from '../../UiGlobals.js';
 import _ from 'lodash';
@@ -20,16 +22,16 @@ const
 	PDF = 'PDF',
 	EXCEL = 'PhpOffice';
 
-export default function Report(props) {
+function Report(props) {
 	if (CURRENT_MODE !== UI_MODE_WEB) {
 		return <Text>Reports are web only!</Text>;
 	}
 	const {
 			title,
 			description,
-			icon,
-			items = [],
 			reportId,
+			// icon,
+			items = [],
 			disablePdf = false,
 			disableExcel = false,
 			includePresets = false,
@@ -43,8 +45,7 @@ export default function Report(props) {
 			const
 				winName = 'ReportWindow',
 				opts = 'resizable=yes,height=600,width=800,location=0,menubar=0,scrollbars=1',
-				params = { 'param1' : '1','param2' :'2'},
-				form = document.createElement("form");
+				form = document.createElement('form');
 			form.setAttribute('method', 'post');
 			form.setAttribute('action', url);
 			form.setAttribute('target', winName);
@@ -61,7 +62,7 @@ export default function Report(props) {
 			form.submit();
 			document.body.removeChild(form);
 		},
-		getReport = (reportType) => {
+		getReport = (reportType, data) => {
 			const params = {
 				report_id: reportId,
 				outputFileType: reportType,
@@ -70,41 +71,56 @@ export default function Report(props) {
 			};
 
 
-			// TODO: add in the values from the form 
-			// (how do I get those without using the Form's buttons?)
-
-
 			openWindowWithPostRequest(params);
 		};
+
+	const propsIcon = props._icon || {};
+	propsIcon.size = 60;
+	propsIcon.px = 5;
+	let icon = props.icon;
+	if (_.isEmpty(icon)) {
+		icon = ChartLine;
+	}
+	if (React.isValidElement(icon)) {
+		if (!_.isEmpty(propsIcon)) {
+			icon = React.cloneElement(icon, {...propsIcon});
+		}
+	} else {
+		icon = <Icon as={icon} {...propsIcon} />;
+	}
+
 	if (!disableExcel) {
-		buttons.push(<Button
-						key="ExcelBtn"
-						onPress={() => getReport(EXCEL)}
-					>Excel</Button>);
+		buttons.push({
+			key: 'ExcelBtn',
+			text: 'Download Excel',
+			// leftIcon: <Icon as={Excel} size="xl" color="#fff" />,
+			onPress: (data) => getReport(EXCEL, data),
+			ml: 1,
+		});
 	}
 	if (!disablePdf) {
-		buttons.push(<Button
-						key="pdfBtn"
-						leftIcon={<Icon as={Pdf} />}
-						onPress={() => getReport(PDF)}
-					>PDF</Button>);
+		buttons.push({
+			key: 'pdfBtn',
+			text: 'Download PDF',
+			leftIcon: <Icon as={Pdf} size="sm" color="#fff" />,
+			onPress: (data) => getReport(PDF, data),
+			ml: 1,
+		});
 	}
-	return <Column w="100%">
+	return <Column w="100%" borderWidth={1} borderColor="primary.300" pt={4}>
 				<Row>
 					{icon && <Column>{icon}</Column>}
 					<Column>
-						<Text fontSize="xl">{title}</Text>
+						<Text fontSize="2xl">{title}</Text>
 						<Text fontSize="sm">{description}</Text>
-						<Form
-							items={items}
-							type={EDITOR_TYPE__PLAIN}
-							
-						/>
 					</Column>
 				</Row>
-
-				<Footer justifyContent="flex-end" alignItems="flex-end">
-					{buttons}
-				</Footer>
+				<Form
+					items={items}
+					type={EDITOR_TYPE__PLAIN}
+					additionalFooterButtons={buttons}
+				/>
 			</Column>;
 }
+
+export default withComponent(Report);
