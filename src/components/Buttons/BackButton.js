5bcfc3f1a0d676a28cd8083012bf28a49d9e4f1c
Scott Spuler <scott@onehat.com>
2023-10-18 11:24:04 -0700

    BackButton

--- src/components/Buttons/BackButton.js
+++ src/components/Buttons/BackButton.js
@@ -6,9 +6,13 @@ import {
 import AngleLeft from '../Icons/AngleLeft.js';
 
 export default function BackButton(props) {
-	return <Pressable flexDirection="row" justifyContent="flex-start" alignItems="center" pr={5} {...props}>
-				<Icon as={AngleLeft} color="#2563eb" size="sm" />
-				<Text fontSize={20} color="#2563eb" left={-1}>Back</Text>
+	const {
+		color = '',
+		...propsToPass
+	} = props;
+	return <Pressable flexDirection="row" justifyContent="flex-start" alignItems="center" pr={5} {...propsToPass}>
+				<Icon as={AngleLeft} color={color} size="sm" />
+				<Text fontSize={20} color={color} left={-1}>Back</Text>
 			</Pressable>;
 }
 
