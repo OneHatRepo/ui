c8d0f26fa387d6d8b5798156881209e4ccaf1e43
Scott Spuler <scott@onehat.com>
2023-10-25 20:32:38 -0700

    Attachments component to use FileCardCustom, bump revision

--- src/PlatformImports/Web/Attachments.js
+++ src/PlatformImports/Web/Attachments.js
@@ -3,7 +3,9 @@ import {
 	Box,
 	Button,
 	Column,
+	Pressable,
 	Row,
+	Text,
 } from 'native-base';
 import {
 	CURRENT_MODE,
@@ -16,6 +18,8 @@ import {
 	FILE_MODE_FILE,
 } from '../../Constants/File.js';
 import { Avatar, Dropzone, FileMosaic, FileCard, FileInputButton, } from "@files-ui/react";
+import IconButton from '@onehat/ui/src/Components/Buttons/IconButton.js';
+import Xmark from '@onehat/ui/src/Components/Icons/Xmark.js'
 import withAlert from '../../Components/Hoc/withAlert.js';
 import withData from '../../Components/Hoc/withData.js';
 import _ from 'lodash';
@@ -23,6 +27,46 @@ import _ from 'lodash';
 const
 	EXPANDED_MAX = 100,
 	COLLAPSED_MAX = 2;
+
+const downloadWithFetch = (url, options = {}) => {
+	fetch(url, options)
+		.then( res => res.blob() )
+		.then( blob => {
+			const
+				winName = 'ReportWindow',
+				opts = 'resizable=yes,height=600,width=800,location=0,menubar=0,scrollbars=1',
+				externalWindow = window.open('', winName, opts),
+				file = externalWindow.URL.createObjectURL(blob);
+			externalWindow.location.assign(file);
+		});
+};
+
+function FileCardCustom(props) {
+	const
+		{
+			id,
+			name: filename,
+			type: mimetype,
+			onDelete,
+			downloadUrl,
+		} = props;
+	return <Pressable
+				px={3}
+				py={1}
+				alignItems="center"
+				flexDirection="row"
+				borderRadius={5}
+				borderWidth={1}
+				borderColor="primary.700"
+				onPress={() => {
+					downloadWithFetch(downloadUrl);
+				}}
+			>
+				<Text>{filename}</Text>
+				<IconButton icon={Xmark} onPress={() => onDelete(id)} />
+			</Pressable>;
+}
+	
 
 // Note this component uploads only one file per server request---
 // it doesn't upload multiple files simultaneously.
@@ -37,6 +81,7 @@ function AttachmentsElement(props) {
 			canCrud = true,
 			_dropZone = {},
 			_fileMosaic = {},
+			useFileMosaic = true,
 			accept, // 'image/*'
 			maxFiles = null,
 			disabled = false,
@@ -200,7 +245,6 @@ function AttachmentsElement(props) {
 	}
 	let content = <Column
 						w="100%"
-						// minHeight={50}
 						p={2}
 						background={styles.ATTACHMENTS_BG}
 					>
@@ -210,11 +254,18 @@ function AttachmentsElement(props) {
 											key={file.id}
 											marginRight={4}
 										>
-											<FileMosaic
-												{...file}
-												backgroundBlurImage={false}
-												{..._fileMosaic}
-											/>
+											{useFileMosaic &&
+												<FileMosaic
+													{...file}
+													backgroundBlurImage={false}
+													{..._fileMosaic}
+												/>}
+											{!useFileMosaic &&
+												<FileCardCustom
+													{...file}
+													backgroundBlurImage={false}
+													{..._fileMosaic}
+												/>}
 										</Box>;
 							})}
 						</Row>
