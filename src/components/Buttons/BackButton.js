e0edc262dc4c8c6baa8efcc001a98ca0798be58d
Scott Spuler <scott@onehat.com>
2023-10-31 10:35:50 -0700

    BackButton mr, bump revision

--- src/components/Buttons/BackButton.js
+++ src/components/Buttons/BackButton.js
@@ -11,7 +11,7 @@ export default function BackButton(props) {
 		...propsToPass
 	} = props;
 	return <Pressable flexDirection="row" justifyContent="flex-start" alignItems="center" pr={5} {...propsToPass}>
-				<Icon as={AngleLeft} color={color} size="sm" />
+				<Icon as={AngleLeft} color={color} size="sm" mr={1} />
 				<Text fontSize={20} color={color} left={-1}>Back</Text>
 			</Pressable>;
 }
