03485b7e904b88a0b715625c85d4f1ecd9d32689
Scott Spuler <scott@onehat.com>
2023-12-08 06:02:09 -0800

    fix SquareButton

--- src/components/Buttons/SquareButton.js
+++ src/components/Buttons/SquareButton.js
@@ -4,12 +4,14 @@ import {
 	Pressable,
 	Text,
 } from 'native-base';
+import IconButton from './IconButton.js';
 import UiGlobals from '../../UiGlobals.js';
 
 export default function SquareButton(props) {
 	const {
 			text,
 			isActive = false,
+			showText = true,
 			activeColor,
 			invertColorWhenActive = false,
 			disableInteractions = false,
@@ -17,7 +19,9 @@ export default function SquareButton(props) {
 			...propsToPass
 		} = props,
 		styles = UiGlobals.styles,
+		bg = isActive ? activeColor || '#56a6f8' : '#fff',
 		color = invertColorWhenActive && isActive ? '#fff' : '#000';
+
 	const propsIcon = props._icon || {};
 	let icon = props.icon;
 	if (!icon) {
@@ -25,14 +29,6 @@ export default function SquareButton(props) {
 	}
 	if (!text) {
 		throw Error('text missing');
-	}
-	
-	if (React.isValidElement(icon)) {
-		if (!_.isEmpty(propsIcon)) {
-			icon = React.cloneElement(icon, {...propsIcon});
-		}
-	} else {
-		icon = <Icon as={icon} {...propsIcon} />;
 	}
 
 	const
@@ -42,7 +38,29 @@ export default function SquareButton(props) {
 		hoverProps.bg = styles.ICON_BUTTON_BG_HOVER;
 		pressedProps.bg = styles.ICON_BUTTON_BG_PRESSED;
 	}
-	
+
+	if (!showText) {
+		return <IconButton
+					icon={icon}
+					borderRadius="md"
+					p={2}
+					_icon={{
+						size: '20px',
+						color,
+					}}
+					{...propsToPass}
+					bg={bg}
+				/>;
+	}
+
+	if (React.isValidElement(icon)) {
+		if (!_.isEmpty(propsIcon)) {
+			icon = React.cloneElement(icon, {...propsIcon});
+		}
+	} else {
+		icon = <Icon as={icon} {...propsIcon} />;
+	}
+
 	return <Pressable
 				borderRadius="md"
 				flexDirection="column"
@@ -50,7 +68,7 @@ export default function SquareButton(props) {
 				alignItems="center"
 				p={2}
 				{...propsToPass}
-				bg={isActive ? activeColor || '#56a6f8' : '#fff'}
+				bg={bg}
 				// _hover={hoverProps}
 				// _pressed={pressedProps}
 			>
