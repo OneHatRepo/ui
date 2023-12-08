4cbd4af9533320ff3d69332893b8eb5b0278f0a8
Scott Spuler <scott@onehat.com>
2023-12-08 13:51:01 -0800

    refactor ErrorMessage, bump revision

--- src/components/Messages/ErrorMessage.js
+++ src/components/Messages/ErrorMessage.js
@@ -6,10 +6,27 @@ import {
 import TriangleExclamation from '../Icons/TriangleExclamation.js';
 
 export default function ErrorMsg(props) {
-	return <Row justifyContent="center" alignItems="center" my={2} w="100%">
-				<Icon as={TriangleExclamation} color="red.500" size="sm" mr={1} />
-				<Text color="red.500">
-					{props.children}
-				</Text>
-			</Row>;
+	const {
+			textMessage = 'Error',
+			textColor = 'red.500',
+		} = props;
+
+	return <Modal {...props} _backdrop={{ bg: "#000" }}>
+				<Modal.Content>
+					<Modal.Body
+						borderTopWidth={0}
+						bg="#fff"
+						p={3}
+						justifyContent="center"
+						alignItems="center"
+						borderRadius={5}
+						flexDirection="row"
+					>
+						<Row justifyContent="center" alignItems="center" my={2} w="100%">
+							<Icon as={TriangleExclamation} color="red.500" size="sm" mr={1} />
+							<Text color={textColor}>{textMessage}</Text>
+						</Row>
+					</Modal.Body>
+				</Modal.Content>
+			</Modal>;
 }
