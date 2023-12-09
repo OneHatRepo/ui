4f0c91dce9ef8106aafc55998bfadfefeeec974f
Scott Spuler <scott@onehat.com>
2023-12-09 13:29:07 -0800

    Loading - when isScreen, show "Loading" text

--- src/components/Messages/Loading.js
+++ src/components/Messages/Loading.js
@@ -1,13 +1,17 @@
 import {
 	Row,
 	Spinner,
+	Text,
 } from 'native-base';
 import ScreenContainer from '../Container/ScreenContainer.js';
 
 export default function Loading(props) {
 	if (props.isScreen) {
 		return <ScreenContainer {...props}>
-					<Spinner flex={1} color="primary.500" />
+					<Row flex={1} justifyContent="center" alignItems="center">
+						<Spinner color="primary.500" mr={2} />
+						<Text>Loading</Text>
+					</Row>
 				</ScreenContainer>;
 	}
 	return <Row justifyContent="center" minHeight={100} {...props}>
