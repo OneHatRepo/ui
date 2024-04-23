12f3b8e55d03b5c4bf1f4fe018a1afb07eea0e67
Scott Spuler <scott@onehat.com>
2024-04-23 10:45:18 -0700

    autoSubmit TextArea after delay, bump revision

--- src/components/Form/Field/TextArea.js
+++ src/components/Form/Field/TextArea.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, } from 'react';
 import {
 	TextArea,
 } from 'native-base';
@@ -10,12 +10,49 @@ import _ from 'lodash';
 
 const
 	TextAreaElement = (props) => {
-		const
+		let { // so localValue can be changed, if needed
+				setValue,
+				autoSubmit = true, // automatically setValue after user stops typing for autoSubmitDelay
+				autoSubmitDelay = UiGlobals.autoSubmitDelay,
+				onChangeText,
+			} = props,
+			value = _.isNil(props.value) ? '' : props.value, // null value may not actually reset this TextArea, so set it explicitly to empty string
 			styles = UiGlobals.styles,
-			value = _.isNil(props.value) ? '' : props.value; // null value may not actually reset this TextArea, so set it explicitly to empty string
+			debouncedSetValueRef = useRef(),
+			[localValue, setLocalValue] = useState(value),
+			onChangeTextLocal = (value) => {
+				if (value === '') {
+					value = null; // empty string makes value null
+				}
+				setLocalValue(value);
+				if (autoSubmit) {
+					debouncedSetValueRef.current(value);
+				}
+				if (onChangeText) {
+					onChangeText(value);
+				}
+			};
+		
+		useEffect(() => {
+			// Set up debounce fn
+			// Have to do this because otherwise, lodash tries to create a debounced version of the fn from only this render
+			debouncedSetValueRef.current = _.debounce(setValue, autoSubmitDelay);
+		}, [setValue]);
+
+		useEffect(() => {
+
+			// Make local value conform to externally changed value
+			setLocalValue(value);
+	
+		}, [value]);
+
+		if (localValue === null || typeof localValue === 'undefined') {
+			localValue = ''; // If the value is null or undefined, don't let this be an uncontrolled input
+		}
+
 		return <TextArea
 					ref={props.outerRef}
-					onChangeText={props.setValue}
+					onChangeText={onChangeTextLocal}
 					flex={1}
 					bg={styles.FORM_TEXTAREA_BG}
 					_focus={{
@@ -24,7 +61,7 @@ const
 					fontSize={styles.FORM_TEXTAREA_FONTSIZE}
 					h={styles.FORM_TEXTAREA_HEIGHT}
 					{...props}
-					value={value}
+					value={localValue}
 				/>;
 	},
 	TextAreaField = withComponent(withValue(TextAreaElement));
