b7439a80731ce4b7e3c3887c69fe6df91d3ab986
Scott Spuler <scott@onehat.com>
2024-04-24 13:23:08 -0700

    registerComponents - change merge to assign

--- src/functions/registerComponents.js
+++ src/functions/registerComponents.js
@@ -2,5 +2,5 @@ import UiGlobals from '../UiGlobals.js';
 import _ from 'lodash';
 
 export default function registerComponents(newComponents) {
-	_.merge(UiGlobals.components, newComponents);
+	_.assign(UiGlobals.components, newComponents); // use assign instead of merge, so that it overwrites existing properties; however this does not work recursively
 }
