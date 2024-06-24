298c5ba42ad0156d840e768c76c65357c2c5e563
Scott Spuler <scott@onehat.com>
2024-06-24 12:17:57 -0700

    fix ReloadButton, bump revision

--- src/components/Buttons/ReloadButton.js
+++ src/components/Buttons/ReloadButton.js
@@ -5,11 +5,12 @@ import testProps from '../../Functions/testProps.js';
 import IconButton from './IconButton.js';
 import Rotate from '../Icons/Rotate.js';
 
-export default function ReloadButton(props, isTree = false) {
+export default function ReloadButton(props) {
 	const {
 			iconProps ={},
 			self,
 			Repository,
+			isTree = false,
 		} = props,
 		onPress = () => {
 			if (isTree) {
