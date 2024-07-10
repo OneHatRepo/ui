054f5094073399e37b6844c48b4edc1c1b4b1d10
Scott Spuler <scott@onehat.com>
2024-07-10 10:51:35 -0700

    improve Grid error message, bump revision

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -225,7 +225,7 @@ function GridComponent(props) {
 					const usedIds = [];
 					_.each(localConfig, (column, ix) => {
 						if (!column.id || inArray(column.id, usedIds)) {
-							throw Error('When using unserializable columns, each column must have a unique id');
+							throw Error('When using unserializable columns, each column must have a unique id. First invalid column: ' + JSON.stringify(column));
 						}
 						usedIds.push(column.id);
 						localConfig[ix] = {
