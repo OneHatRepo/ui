c05eda642a00aeb3fc29c528c1600ef79ef90e0c
Scott Spuler <scott@onehat.com>
2024-07-20 08:35:41 -0700

    jsonValidator to handle required

--- src/functions/jsonValidator.js
+++ src/functions/jsonValidator.js
@@ -1,13 +1,18 @@
 import * as yup from 'yup';
+import _ from 'lodash';
 
-export default function json() {
+export default function json(isRequired = false) {
 	return yup.mixed().test(
 		'is-json',
 		'${path} is not valid JSON',
 		value => {
 			try {
-				JSON.parse(value);
-				return true; // Valid JSON
+				const json = JSON.parse(value);
+				// Valid JSON
+				if (isRequired && _.isEmpty(json)) {
+					return false; // Empty JSON
+				}
+				return true;
 			} catch (error) {
 				return false; // Invalid JSON
 			}
