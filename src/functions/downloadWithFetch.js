cca77e2f44433688d969dc40487e5cdf22369b72
Scott Spuler <scott@onehat.com>
2024-09-25 14:12:03 -0700

    refactor downloadWithFetch, bump revision

--- src/functions/downloadWithFetch.js
+++ src/functions/downloadWithFetch.js
@@ -3,16 +3,33 @@ const downloadWithFetch = (url, options = {}, win = null) => {
 	fetch(url, options)
 		.then( res => res.blob() )
 		.then( blob => {
-			if (!win) {
-				const
-					winName = 'Download',
-					opts = 'location=0,menubar=0,scrollbars=0';
-				win = window.open('', winName, opts);
-			}
+			// if (!win) {
+			// 	const
+			// 		winName = 'Download',
+			// 		opts = 'location=0,menubar=0,scrollbars=0';
+			// 	win = window.open('about:blank', winName, opts);
+			// }
 
-			const file = win.URL.createObjectURL(blob);
-			obj.window = win;
-			win.location.assign(file);
+			// const file = win.URL.createObjectURL(blob);
+			// obj.window = win;
+			const file = URL.createObjectURL(blob);
+
+			// const link = win.document.createElement('a');
+			const link = document.createElement('a');
+			link.href = file;
+			// link.download = true;
+			link.target = "_blank";
+			link.click();
+
+
+			// win.URL.revokeObjectURL(file);
+
+			// const newWin = win.open(file);
+			// win.location.assign(file);
+			// setTimeout(() => {
+			//	win.close();
+			// }, 2000);
+
 		});
 	return obj;
 };
