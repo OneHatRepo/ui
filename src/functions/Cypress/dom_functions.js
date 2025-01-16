a65141637b65c1a0235adcacb40f0fabf08446bc
Scott Spuler <scott@onehat.com>
2025-01-16 12:09:00 -0800

    drag Cypress fn WIP

--- src/functions/Cypress/dom_functions.js
+++ src/functions/Cypress/dom_functions.js
@@ -24,9 +24,11 @@ export function getDomNodes(selectors, options = {}) {
 
 /**
  * Builds selector string for data-testid attributes.
- * @argument {string | string[]} selectors - data-testid attribute values
- * If an array is given, these will be considered nested selectors.
+ * It leaves classname, id, and attribute selectors unchanged.
+ * 
+ * If selectors is an array, these will be considered nested selectors.
  * e.g. ['parent', 'child'] will be converted to '[data-testid="parent"] [data-testid="child"]'
+ * @argument {string|string[]} selectors - data-testid attribute values
  * @return {string}
  */
 export function getTestIdSelectors(selectors, isGetFirst = false) {
@@ -34,16 +36,47 @@ export function getTestIdSelectors(selectors, isGetFirst = false) {
 		selectors = [selectors];
 	}
 	const selectorParts = _.map(selectors, (selector) => {
-		if (selector.match(/=/)) { // selector is something like [role="switch"], so don't use data-testid
-			return selector;
+		if (!selector.match(/^\./) // className, like .my-class
+			&& !selector.match(/^#/) // id, like @my-id
+			&& !selector.match(/=/) // attribute, like [role="switch"]
+		){
+			selector = '[data-testid="' + selector + '"]';
 		}
-		if (selector.match(/^\./)) { // selector is something like .my-class, so don't use data-testid
-			return selector;
+		if (isGetFirst) {
+			selector += ':first';
 		}
-		if (selector.match(/^#/)) { // selector is something like #my-id, so don't use data-testid
-			return selector;
-		}
-		return '[data-testid="' + selector + '"]' + (isGetFirst ? ':first' : '');
+		return selector;
 	});
 	return selectorParts.join(' ');
+}
+
+export function drag(draggableSelectors, droppableSelectors, options = {}) {
+	if (typeof options.force === 'undefined') {
+		options.force = true;
+	}
+
+	// getDomNode(getTestIdSelectors(droppableSelectors)).then((node) => {
+	// 	const selectors = getTestIdSelectors(droppableSelectors);
+	// 	debugger;
+	// });
+
+	options = {
+		source: { // applies to the element being dragged
+			x: 10,
+			// y: 100,
+			position: 'left',
+		},
+		target: { // applies to the drop target
+			position: 'left',
+			x: 20,
+		},
+		force: true, // applied to both the source and target element
+	};
+
+
+	return getDomNode(draggableSelectors)
+			.drag(getTestIdSelectors(droppableSelectors), options)
+			.then((success) => {
+				debugger;
+			});
 }
