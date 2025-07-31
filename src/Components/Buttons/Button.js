dde28e99b10f56a7e8662032d4f4881cfbee8422
Scott Spuler <scott@onehat.com>
2025-07-31 13:26:25 -0700

    addIconProps, integration with Native

--- src/Components/Buttons/Button.js
+++ src/Components/Buttons/Button.js
@@ -6,6 +6,11 @@ import {
 	ButtonIcon,
 	ButtonGroup,
 } from '@project-components/Gluestack';
+import {
+	UI_MODE_WEB,
+	UI_MODE_NATIVE,
+	CURRENT_MODE,
+} from '../../Constants/UiModes.js';
 import clsx from 'clsx';
 import withComponent from '../Hoc/withComponent.js';
 import withTooltip from '../Hoc/withTooltip.js';
@@ -25,22 +30,60 @@ const ButtonComponent = forwardRef((props, ref) => {
 			...propsToPass
 		} = props;
 	
+
+	function addIconProps(iconProps = {}) {
+
+		iconProps.className = clsx(
+			'ButtonIcon',
+			iconProps.className,
+		);
+
+		if (CURRENT_MODE === UI_MODE_WEB) {
+			return iconProps;
+		}
+
+		// native only
+
+		// marginx
+		iconProps.style = {
+			marginRight: 8,
+			marginLeft: 8,
+			...iconProps.style,
+		};
+
+		// On native, react-native-svg ignores className and will only size the icon based on 
+		// explicit width / height props (or size if the wrapper supports it).
+		// If no size set, it falls back to the full intrinsic viewBox size, so we need to ensure we set a default size.
+		// If you want to override the size, pass width and height props to the icon.
+		if (iconProps.width || iconProps.height) {
+			return iconProps;
+		}
+		const nativeDefaults = {
+			width: 24,
+			height: 24,
+		};
+		return {
+			...nativeDefaults,
+			...iconProps,
+		};
+	};
+
 	if (icon) {
 		if (isValidElement(icon)) {
 			if (_icon) {
-				icon = cloneElement(icon, {..._icon});
+				icon = cloneElement(icon, addIconProps(_icon || {}));
 			}
 		} else {
-			icon = <ButtonIcon className="ButtonIcon" as={icon} {..._icon} />;
+			icon = <ButtonIcon as={icon} {...addIconProps(_icon || {})} />;
 		}
 	}
 	if (rightIcon) {
 		if (isValidElement(rightIcon)) {
 			if (_rightIcon) {
-				rightIcon = cloneElement(rightIcon, {..._rightIcon});
+				rightIcon = cloneElement(rightIcon, addIconProps(_rightIcon || {}));
 			}
 		} else {
-			rightIcon = <ButtonIcon className="ButtonIcon" as={rightIcon} {..._rightIcon} />;
+			rightIcon = <ButtonIcon as={rightIcon} {...addIconProps(_rightIcon || {})} />;
 		}
 	}
 	
@@ -60,7 +103,12 @@ const ButtonComponent = forwardRef((props, ref) => {
 		className += ' ' + propsToPass.className;
 	}
 	
-	return <Button {...propsToPass} className={className} ref={ref}>
+	return <Button
+				{...propsToPass}
+				className={className}
+				ref={ref}
+				style={[{ flexDirection: 'row', alignItems: 'center' }, propsToPass.style]}
+			>
 				{isLoading && <ButtonSpinner className="ButtonSpinner" {..._spinner} />}
 				{icon}
 				{text && <ButtonText className="ButtonText" {..._text}>{text}</ButtonText>}
