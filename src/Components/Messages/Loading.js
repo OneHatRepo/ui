f60f2db6326e451eea68639a702778a3fc408437
Scott Spuler <scott@onehat.com>
2025-08-27 16:18:35 -0700

    Loading enhancements, more Dates constants

--- src/Components/Messages/Loading.js
+++ src/Components/Messages/Loading.js
@@ -10,14 +10,14 @@ import ScreenContainer from '../Container/ScreenContainer.js';
 export default function Loading(props) {
 
 	if (props.isScreen) {
-		return <ScreenContainer {...props}>
+		return <ScreenContainer className="h-full w-full" {...props}>
 					<HStack className="flex-1 justify-center items-center">
 						<Spinner className="text-primary-500 mr-1" />
 						<Text>Loading</Text>
 					</HStack>
 				</ScreenContainer>;
 	}
-	return <HStackNative {...props} className="justify-center min-h-[100px]">
+	return <HStackNative {...props} className="justify-center min-h-[100px] h-full w-full">
 				<Spinner className="flex-1 text-primary-500" />
 			</HStackNative>;
 }
