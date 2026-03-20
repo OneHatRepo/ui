2abd9be9d5a27c6a0a117fefba84573e8f5a191a
Scott Spuler <scott@onehat.com>
2026-03-20 08:29:15 -0700

    SetupButton to match coloring of UserIndicator, also to have bg-red when isSetupMode

--- src/Components/Layout/SetupButton.js
+++ src/Components/Layout/SetupButton.js
@@ -3,6 +3,7 @@ import {
 	selectIsSetupMode,
 	toggleSetupMode,
 } from '@src/Models/Slices/AppSlice';
+import clsx from 'clsx';
 import Button from '../Buttons/Button';
 import IconButton from '../Buttons/IconButton';
 import Gear from '../Icons/Gear';
@@ -13,19 +14,44 @@ export default function SetupButton(props) {
 		} = props,
 		dispatch = useDispatch(),
 		isSetupMode = useSelector(selectIsSetupMode),
-		onPress = () => dispatch(toggleSetupMode());
+		onPress = () => dispatch(toggleSetupMode()),
+		buttonClassName = clsx(
+			'SetupButton',
+			isSetupMode
+				? 'bg-red-500 data-[hover=true]:bg-red-600 data-[active=true]:bg-red-700'
+				: 'bg-grey-100 data-[hover=true]:bg-grey-900/20 data-[active=true]:bg-grey-900/50',
+		),
+		textClassName = clsx(
+			isSetupMode
+				? 'text-white data-[hover=true]:text-white data-[active=true]:text-white'
+				: 'text-black data-[hover=true]:text-black data-[active=true]:text-black',
+		),
+		iconClassName = clsx(
+			isSetupMode ? 'fill-white' : 'fill-black',
+			isSetupMode ? 'text-white' : 'text-black',
+		);
+
 	return isMinimized ? 
 			<IconButton
 				icon={Gear}
+				_icon={{
+					className: iconClassName,
+				}}
 				onPress={onPress}
 				tooltip="Toggle Setup Mode"
-				className="SetupButton-IconButton"
+				className={buttonClassName}
 			/> : 
 			<Button
 				text={isSetupMode ? 'Exit Setup' : 'Setup'}
 				icon={Gear}
+				_text={{
+					className: textClassName,
+				}}
+				_icon={{
+					className: iconClassName,
+				}}
 				onPress={onPress}
 				tooltip="Toggle Setup Mode"
-				className="SetupButton-Button"
+				className={buttonClassName}
 			/>;
 };
