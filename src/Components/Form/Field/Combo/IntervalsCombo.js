2506eea7e8461b37dff176c1125042a8cd3dfa87
Scott Spuler <scott@onehat.com>
2026-06-13 06:20:58 -0700

    IntervalsCombo

--- src/Components/Form/Field/Combo/IntervalsCombo.js
+++ src/Components/Form/Field/Combo/IntervalsCombo.js
@@ -1,12 +1,12 @@
 import ArrayCombo from './ArrayCombo.js';
 
 const data = [
-	['+1 day', 'Daily'], 
-	['+1 week', 'Weekly'], 
-	// ['+2 week', 'Bi-weekly'], // ambibuous, don't use
-	['+1 month', 'Monthly'], 
-	['+1 quarter', 'Quarterly'], 
-	['+1 year', 'Yearly']
+	['1 day', 'Daily'], 
+	['1 week', 'Weekly'], 
+	// ['2 week', 'Bi-weekly'], // ambibuous, don't use
+	['1 month', 'Monthly'], 
+	['1 quarter', 'Quarterly'], 
+	['1 year', 'Yearly']
 ];
 
 export default function IntervalsCombo(props) {
