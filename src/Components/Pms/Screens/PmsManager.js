1d487a20ad1b0b18002d808a22870862e8651801
Scott Spuler <scott@onehat.com>
2026-08-11 06:32:21 -0700

    fix PmsManager, bump revision

--- src/Components/Pms/Screens/PmsManager.js
+++ src/Components/Pms/Screens/PmsManager.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, } from 'react';
 import { useSelector } from 'react-redux';
-import useRouteIsFocused from '../../Hooks/useRouteIsFocused';
+import useRouteIsFocused from '../../../Hooks/useRouteIsFocused';
 import {
 	selectTreeSelection,
 } from '@src/Models/Slices/AppSlice';
