729ae5d7c981b60e67621c909aa898b0d249f12f
Scott Spuler <scott@onehat.com>
2026-09-24 07:50:52 -0700

    fix crud_functions

--- src/Functions/Cypress/crud_functions.js
+++ src/Functions/Cypress/crud_functions.js
@@ -536,7 +536,7 @@ export function addGridRecord(args) {
 	cy.wait(1000); // allow temp id to be replaced by real one
 
 	// Get and save id of new record
-	getDomNode([selector, 'Row_row-selected']).then((row) => {
+	getDomNode([selector, 'Row-selected']).then((row) => {
 		const parent = row[0].parentNode;
 		cy.wrap(parent).invoke('attr', 'data-testid').then((testId) => {
 			const id = testId.split('-')[1];
